873409ca4a5b809599b4e40e4690bb850fcb250f
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2006-03-02 14:40:51 +0000

    added test infrastructure for testing web server
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/Pootle@2067 54714841-351b-0410-a198-e36a94b762f5

---
+++ test_client.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+"""Top-level tests using urllib2 to check interaction with servers
+This uses test_cmdlineserver / test_service / test_apache (which are for different webservers)
+ServerTester is mixed in with the different server tests below to generate the required tests"""
+
+from Pootle import test_create
+from Pootle import test_cmdlineserver
+import urllib2
+try:
+	import cookielib
+except ImportError:
+	# fallback for python before 2.4
+	cookielib = None
+	import ClientCookie
+
+class ServerTester:
+	"""Tests things directly against the socket of the server. Requires self.baseaddress"""
+	setup_class = test_create.TestCreate.setup_class
+
+	def setup_cookies(self):
+		"""handle cookies etc"""
+		if cookielib:
+			self.cookiejar = cookielib.CookieJar()
+			self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookiejar))
+			self.urlopen = self.opener.open
+		else:
+			self.urlopen = ClientCookie.urlopen
+
+	def fetch_page(self, relative_url):
+		"""Fetches a page from the webserver installed in the service"""
+		url = "%s/%s" % (self.baseaddress, relative_url)
+		print "fetching", url
+		stream = self.urlopen(url)
+		contents = stream.read()
+		return contents
+
+	def login(self):
+		"""calls the login method with username and password"""
+		return self.fetch_page("?islogin=1&username=admin&password=")
+
+	def test_login(self):
+		"""checks that login works and sets cookies"""
+		contents = self.login()
+		# check login leads us to a normal page
+		assert "Login" not in contents
+		# check login is retained on next fetch
+		contents = self.fetch_page("")
+		assert "Login" not in contents
+
+	def setup_prefs(self, method):
+		"""sets up any extra preferences required..."""
+		pass
+
+def MakeServerTester(baseclass):
+	"""Makes a new Server Tester class using the base class to setup webserver etc"""
+	class TestServer(baseclass, ServerTester):
+		def setup_method(self, method):
+			ServerTester.setup_prefs(self, method)
+			baseclass.setup_method(self, method)
+			ServerTester.setup_cookies(self)
+	return TestServer
+
+TestServerCmdLine = MakeServerTester(test_cmdlineserver.TestCmdlineServer)
+# TestServerService = MakeServerTester(test_service.TestjLogbookService)
+# TestServerApache = MakeServerTester(test_apache.TestApachejLogbook)
+
