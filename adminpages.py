8443b1ab9978dd979f2e730c2a5d0cac8f3e3377
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-04-05 07:00:39 +0000

    Refix language admin page with existing variables
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/Pootle@2250 54714841-351b-0410-a198-e36a94b762f5

--- adminpages.py
+++ adminpages.py
@@ -85,7 +85,7 @@ class LanguagesAdminPage(pagelayout.PootlePage):
                {"name": "specialchars", "title": self.localize("Special Chars"), "newvalue": self.localize("(special characters)")},
                {"name": "nplurals", "title": self.localize("Number of Plurals"), "newvalue": self.localize("(number of plurals)")},
                {"name": "pluralequation", "title": self.localize("Plural Equation"), "newvalue": self.localize("(plural equation)")},
-               {"name": "remove", "title": self.localize("Remove Language"), "existingonly": True}]
+               {"name": "remove", "title": self.localize("Remove Language")}]
     for option in options:
       if "newvalue" in option:
         option["newname"] = "newlanguage" + option["name"]
@@ -130,9 +130,9 @@ class LanguagesAdminPage(pagelayout.PootlePage):
     rownum = languages.maxrownum()+1
     colnum = 0
     for option in self.getoptions():
-      if "existingonly" in option and option["existingonly"]:
-        continue
-      if "newname" in option:
+      if "newvalue" not in option:
+        optionwidget = widgets.PlainContents("asdfasdadsfasdfasd")
+      elif "newname" in option:
         inputoptions = {"name": option["newname"], "value": option["newvalue"]}
         if "size" in option:
           inputoptions["size"] = option["size"]
