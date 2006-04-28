427d9382a8a64763f0609680e123be0c0a9ac3ab
davidfraser <davidfraser@54714841-351b-0410-a198-e36a94b762f5>
2006-04-28 10:14:10 +0000

    added ability to measure stats creation and index creation this uses a Dummy PO Tree (need to move into potree) index testing should be more robust in particular also would be nice to be able to measure stats creation and index creation without reparsing etc this may require a refactor
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/Pootle@2368 54714841-351b-0410-a198-e36a94b762f5

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from Pootle import pootlefile
+from Pootle import projects
 from translate.storage import po
+from jToolkit.data import indexer
 import os
 import profile
 import pstats
@@ -54,18 +56,76 @@ class PootleBenchmarker:
                 count += len(parsedfile.units)
         print "counted %d elements" % count
 
+    def parse_and_create_stats(self):
+        """parses all the po files in the test directory into memory, using pootlefile, which creates Stats"""
+        count = 0
+        indexer.HAVE_INDEXER = False
+        for dirpath, subdirs, filenames in os.walk(self.test_dir, topdown=False):
+            for name in filenames:
+                pofilename = os.path.join(dirpath, name)
+                parsedfile = pootlefile.pootlefile(pofilename=pofilename, stats=True)
+                count += len(parsedfile.units)
+        print "stats on %d elements" % count
+
+    def parse_and_create_index(self):
+        """parses all the po files in the test directory into memory, using pootlefile, and allow index creation"""
+        count = 0
+        class DummyPoTree:
+            def __init__(self, podir):
+                self.podirectory = podir
+            def getlanguagename(self, languagecode):
+                return languagecode
+            def getprojectname(self, projectcode):
+                return projectcode
+            def getprojectdescription(self, projectcode):
+                return projectcode
+            def getprojectcheckerstyle(self, projectcode):
+                return ""
+            def getpodir(self, languagecode, projectcode):
+                return self.podirectory
+            def hasgnufiles(self, podir, languagecode):
+                return False
+            def getprojectcreatemofiles(self, projectcode):
+                return False
+            def getpofiles(self, languagecode, projectcode, poext):
+                pofiles = []
+                for dirpath, subdirs, filenames in os.walk(self.podirectory, topdown=False):
+                    if dirpath == self.podirectory:
+                        subdirpath = ""
+                    else:
+                        subdirpath = dirpath.replace(self.podirectory+os.path.sep, "", 1)
+                    print dirpath, subdirpath, self.podirectory
+                    pofiles.extend([os.path.join(subdirpath, name) for name in filenames if name.endswith(poext)])
+                return pofiles
+            def gettemplates(self, projectcode):
+                return []
+            def languagematch(self, languagecode, filename):
+                return True
+        indexer.HAVE_INDEXER = True
+        project = projects.TranslationProject("zxx", "benchmark", DummyPoTree(self.test_dir))
+        for name in project.browsefiles():
+            count += len(project.getpofile(name).units)
+        print "indexed %d elements" % count
+        assert os.path.exists(os.path.join(self.test_dir, ".poindex-%s-%s" % (project.projectcode, project.languagecode)))
+
 if __name__ == "__main__":
     for sample_file_sizes in [
       (1, 1, 1, 1, 1),
       (1, 10, 10, 10, 10),
-      (10, 20, 100, 10, 10),
+      (5, 10, 10, 10, 10),
+      # (5, 10, 100, 20, 20),
+      # (10, 20, 100, 10, 10),
       ]:
-        print "%d dirs, %d files, %d strings, %d/%d words" % sample_file_sizes
         benchmarker = PootleBenchmarker("BenchmarkDir")
         benchmarker.clear_test_dir()
         benchmarker.create_sample_files(*sample_file_sizes)
-        profile.run('benchmarker.parse_po_files()', 'Benchmark.stats')
-        stats = pstats.Stats('Benchmark.stats')
-        stats.sort_stats('cumulative').print_stats(10)
+        for methodname in ("parse_po_files", "parse_and_create_stats", "parse_and_create_index"):
+            print methodname, "%d dirs, %d files, %d strings, %d/%d words" % sample_file_sizes
+            print "_______________________________________________________"
+            statsfile = methodname + '_%d_%d_%d_%d_%d.stats' % sample_file_sizes
+            profile.run('benchmarker.%s()' % methodname, statsfile)
+            stats = pstats.Stats(statsfile)
+            stats.sort_stats('cumulative').print_stats(10)
+            print "_______________________________________________________"
         benchmarker.clear_test_dir()
 
