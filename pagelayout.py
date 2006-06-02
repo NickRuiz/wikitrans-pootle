dfeaa65527cde53f7ea1a99a9495cc41a23ee07e
friedelwolff <friedelwolff@54714841-351b-0410-a198-e36a94b762f5>
2006-06-02 09:32:54 +0000

    Internationalise side links
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/Pootle@2643 54714841-351b-0410-a198-e36a94b762f5

--- pagelayout.py
+++ pagelayout.py
@@ -12,6 +12,22 @@ def layout_banner(maxheight):
   return {"logo_width": logo_width, "logo_height": logo_height,
     "banner_width": banner_width, "banner_height": banner_height, "logo_image": logo_image}
 
+def localize_links(session):
+  """Localize all the generic links"""
+  links = {}
+  links["home"] = session.localize("Home")
+  links["projects"] = session.localize("All projects")
+  links["languages"] = session.localize("All languages")
+  links["account"] = session.localize("My account")
+  links["admin"] = session.localize("Admin")
+  links["doc"] = session.localize("Docs & help")
+  links["logout"] = session.localize("Log out")
+  links["login"] = session.localize("Log in")
+  #l10n: Verb, as in "to register"
+  links["register"] = session.localize("Register")
+  links["activate"] = session.localize("Activate")
+  return links
+
 def completetemplatevars(templatevars, session, bannerheight=135):
   """fill out default values for template variables"""
   if not "instancetitle" in templatevars:
@@ -22,6 +38,7 @@ def completetemplatevars(templatevars, session, bannerheight=135):
   banner_layout["logo_alttext"] = session.localize("Pootle Logo")
   banner_layout["banner_alttext"] = session.localize("WordForge Translation Project")
   templatevars.update(banner_layout)
+  templatevars["links"] = localize_links(session)
   if "search" not in templatevars:
     templatevars["search"] = None
 
