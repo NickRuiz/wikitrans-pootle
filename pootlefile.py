d8ec3e4781866b04ed86bb8e35577b7e55a3f61b
dwaynebailey <dwaynebailey@54714841-351b-0410-a198-e36a94b762f5>
2006-06-15 14:32:01 +0000

    [Forwardport] Update copyright dates and add missing copyright headers to files.
    git-svn-id: https://translate.svn.sourceforge.net/svnroot/translate/trunk/Pootle@2832 54714841-351b-0410-a198-e36a94b762f5

--- pootlefile.py
+++ pootlefile.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# 
+# Copyright 2004-2006 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """manages po files and their associated files"""
 
@@ -90,7 +108,8 @@ class pootlefile(po.pofile):
     if project is None:
       from Pootle import projects
       self.project = projects.DummyProject(None)
-      self.checker = None
+      #self.checker = None
+      self.checker = self.project.checker
       self.filename = self.pofilename
     else:
       self.project = project
